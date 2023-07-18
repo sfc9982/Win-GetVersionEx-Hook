dbe24166186eca0a5733367a33229ac1f01f37a7
Jackson Chen <26681591+sfc9982@users.noreply.github.com>
2023-07-18 19:36:46 +0800

    Update GetVersion.cpp

--- GetVersion.cpp
+++ GetVersion.cpp
@@ -1,19 +1,33 @@
+// ReSharper disable CppDeprecatedEntity
 #pragma warning(disable : 4996)
 
+#include <cstdio>
+#include <iostream>
 #include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-int __cdecl main()
-{
-	OSVERSIONINFO osvi;
+#include "Shlwapi.h"
+#pragma comment(lib, "Shlwapi.lib")
+
+int __cdecl main() {
+    OSVERSIONINFO osvi;
 
     ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
     osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
 
-    GetVersionEx(&osvi);
+    GetVersionExW(&osvi);
+    DWORD ubr      = 0;
+    DWORD ubr_size = sizeof(ubr);
+    SHRegGetValue(
+            HKEY_LOCAL_MACHINE,
+            L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion",
+            L"UBR",
+            SRRF_RT_REG_DWORD,
+            nullptr,
+            &ubr,
+            &ubr_size);
 
-    printf("Windows Version: %ld.%ld.%ld\n", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber);
-	
+
+    printf("Windows Version: %ld.%ld.%ld.%ld\n", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber, ubr);
+
     return 0;
 }
